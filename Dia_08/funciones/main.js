d1b50010d3ab02b94ec6385e2648b15156e507ee
arnoldbgm <arnold.gallegosm@gmail.com>
2025-05-08 20:34:56 -0500

    JS fundamentos

--- Dia_08/funciones/main.js
+++ Dia_08/funciones/main.js
@@ -22,6 +22,7 @@ function iniciarLaCafetera (){
    console.log("Sirviendo el cafe...");
 }
 
+
 // ¿Como llamamos a la funcion o la ejecutamos?
 /* Para ejecutar una funcion debes de colocar el nombre de esta:
    [NOMBRE DE LA FUNCION]();
@@ -70,3 +71,50 @@ function ingresoBar(nombre, edad){
 }
 
 console.log(ingresoBar("Summy", 12))
+
+/*
+   RETO 01
+   Crear la funcion para calcular el area de un triangulo, esta funcion debe de recibir
+   2 parametros base y altura, y se debe de retornar el area del triangulo
+   Area = (base * altura) / 2
+   Ejemplo: calcularAreaTriangulo(10, 5) => 25
+
+   RETO 02
+   Crear una funcion que convierta de grados centigrados a grados farenheit
+   Esta funcion debe de recibir un parametro que es el valor en grados centigrados
+   y debe de retornar el valor en grados farenheit
+   Formula: F = C * 9/5 + 32
+   Donde F = grados farenheit y C = grados centigrados
+   Ejemplo: convertirGrados(30) => 86
+
+   RETO 03
+   Crear una funcion que reciba 2 parametros, el primero es la placa del auto, y el segundo 
+   es la velocidad del auto, y debe de retornar un mensaje que diga si el auto va a exceso de velocidad o no
+
+   Si la velocidad es mayor a 80 km/h, el auto va a exceso de velocidad, si no, no va a exceso de velocidad
+   Ejemplo: verificarExcesoVelocidad("ABC123", 90) => "El auto ABC123 va a exceso de velocidad"
+   Ejemplo: verificarExcesoVelocidad("ABC123", 70) => "El auto ABC123 no va a exceso de velocidad"
+
+   🌟 RETO EXTRA 03 => Si el auto esta en velocidad => indicar que el auto esta estacionado
+
+*/
+
+function verificarExcesoVelocidad(placa, velocidad){
+   // if(velocidad >80){
+   //    return `El auto con placa ${placa} va a exceso de velocidad 🚓`
+   // } else if(velocidad === 0){
+   //    return `El auto esta estacionado`
+   // } else{
+   //    return `El auto con placa ${placa} no va a exceso de velocidad 💚`
+   // }
+
+   if(velocidad > 80){
+       return `El auto con placa ${placa} va a exceso de velocidad 🚓`
+   } else if ( velocidad < 80 && velocidad > 0){
+       return `El auto con placa ${placa} no va a exceso de velocidad`
+   } else{
+      return `El auto esta parado`
+   }
+}
+
+console.log(verificarExcesoVelocidad("ABC-001", 60))
